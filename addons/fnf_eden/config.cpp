dfa7c017aebd3906ff36c4b354b0632c73e5bce8
Martin <gabemartin64@gmail.com>
2021-06-27 16:28:41 -0500

    Update config.cpp

--- addons/fnf_eden/config.cpp
+++ addons/fnf_eden/config.cpp
@@ -20,29 +20,29 @@ class Cfg3DEN
         displayName = "FNF Properties";
         collapsed = 1;
         class Attributes
-				{
-					class fnf_autoMarkExclude
-					{
-						property = "FNF_MarkingExclude";
-						control = "Checkbox";
-						displayName = "Exclude from Map Auto-Mark";
-						tooltip = "If checked, this object will be excluded from the FNF custom object marking script.";
-						expression = "_this setVariable ['%s',_value];";
-						typeName = "BOOL";
-						condition = "1 - objectControllable - objectVehicle";
-						defaultValue = false;
-					};
-					class fnf_clearInventory
-					{
-						property = "FNF_InventoryAutoClear";
-						control = "Checkbox";
-						displayName = "Clear Inventory";
-						tooltip = "If checked, the vehicle's inventory will be cleared.";
-						expression = "_this setVariable ['%s',_value];";
-						typeName = "BOOL";
-						condition = "objectVehicle";
-						defaultValue = true;
-					};
+		{
+			class fnf_autoMarkExclude
+			{
+				property = "FNF_MarkingExclude";
+				control = "Checkbox";
+				displayName = "Exclude from Map Auto-Mark";
+				tooltip = "If checked, this object will be excluded from the FNF custom object marking script.";
+				expression = "_this setVariable ['%s',_value];";
+				typeName = "BOOL";
+				condition = "1 - objectControllable - objectVehicle";
+				defaultValue = false;
+			};
+			class fnf_clearInventory
+			{
+				property = "FNF_InventoryAutoClear";
+				control = "Checkbox";
+				displayName = "Clear Inventory";
+				tooltip = "If checked, the vehicle's inventory will be cleared.";
+				expression = "_this setVariable ['%s',_value];";
+				typeName = "BOOL";
+				condition = "objectVehicle";
+				defaultValue = true;
+			};
         };
       };
     };
