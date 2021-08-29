4b1ce6de07b0ae23624b9a5a9461ccabacf79d31
Troy <tanaka_s2@yahoo.com>
2021-08-29 14:39:56 -0500

    Add random chance of death upon car or armor destruction

--- FNF_MissionTemplate.VR/description/cfgEventHandlers.hpp
+++ FNF_MissionTemplate.VR/description/cfgEventHandlers.hpp
@@ -6,10 +6,12 @@ class Extended_InitPost_EventHandlers {
   class Car {
     class PHX_NoBitchZone { init = "(_this select 0) allowCrewInImmobile true;"; };
     class PHX_DisableThermals { serverInit = "(_this select 0) disableTIEquipment true;"; };
+	class tnk_EjectCrew { init = "((_this select 0) addMPEventHandler ['MPKilled', {params ['_unit', '_killer', '_instigator', '_useEffects']; if !(isServer) exitWith {}; {_kill = selectRandom [0,1];if (_kill isEqualTo 1) then {_x setDamage 1;} else {moveOut _x;};} forEach crew _unit;}]);};"};
   };
   class Tank {
     class PHX_NoBitchZone { init = "(_this select 0) allowCrewInImmobile true;"; };
     class PHX_DisableThermals { serverInit = "(_this select 0) disableTIEquipment true;"; };
+	class tnk_EjectCrew { init = "((_this select 0) addMPEventHandler ['MPKilled', {params ['_unit', '_killer', '_instigator', '_useEffects']; if !(isServer) exitWith {}; {_kill = selectRandom [0,1];if (_kill isEqualTo 1) then {_x setDamage 1;} else {moveOut _x;};} forEach crew _unit;}]);};"};
   };
   class Helicopter {
     class PHX_DisableThermals { serverInit = "(_this select 0) disableTIEquipment true;"; };
