804cf52998612e240f236d01644dbd764b95f07c
Troy <tanaka_s2@yahoo.com>
2021-09-03 23:40:03 -0500

    Change to cfgUnitInsignia

---
+++ FNF_MissionTemplate.VR/description/cfgUnitInsignia.hpp
@@ -0,0 +1,10 @@
+class CfgUnitInsignia
+{
+    class FNFPatchAdmin
+    {
+        displayName = "FNF Patch - Admin";
+        author = "Friday Night Fight";
+        texture = "server\init\patches\staffPatch.paa";
+        textureVehicle = "";
+    };
+};
