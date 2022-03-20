492384ad89a277a7a0962c1f221e5e7fbd3c4133
IndigoFox <indifox926@gmail.com>
2022-03-20 00:59:41 -0400

    kit updates

--- FNF_MissionTemplate.VR/description/KITS/UNIFORMS/RHS_UNI_NATO_US_MARINES_RECON_2010.hpp
+++ FNF_MissionTemplate.VR/description/KITS/UNIFORMS/RHS_UNI_NATO_US_MARINES_RECON_2010.hpp
@@ -1,32 +1,32 @@
 #include "..\undef.hpp" // Reset defines
 
-#define UNIFORM "rhs_uniform_vkpo_gloves"
+#define UNIFORM "UK3CB_ADM_B_U_CombatUniform_Shortsleeve_01_MAR", "UK3CB_ADM_B_U_CombatUniform_Shortsleeve_01_MAR_WDL_ALT", "UK3CB_ADM_B_U_CombatUniform_Shortsleeve_01_MAR_DES_WDL", "UK3CB_ADM_B_U_CombatUniform_01_MAR_WDL_ALT", "UK3CB_ADM_B_U_CombatUniform_01_MAR_DES_WDL", "UK3CB_ADM_B_U_CombatUniform_Shortsleeve_01_WDL_ALT_MAR"
 
-#define VEST "rhs_6b45_rifleman", "rhs_6b45_rifleman_2"
-#define VEST_LEADER "rhs_6b45_off"
-#define VEST_AR "rhs_6b45_mg"
-#define VEST_MEDIC VEST
+#define VEST "rhsusf_mbav_rifleman"
+#define VEST_LEADER "rhsusf_mbav_grenadier"
+#define VEST_AR "rhsusf_mbav_mg"
+#define VEST_MEDIC "rhsusf_mbav_medic"
 #define VEST_CREWMAN VEST
 #define VEST_PILOT VEST
 
-#define HELMET "rhs_6b47_bala" "rhs_6b47_6m2", "rhs_6b47_6m2_1"
-#define HELMET_CMDR "rhs_vkpo_cap_alt"
-#define HELMET_RECON "rhs_Booniehat_digi"
-#define HELMET_CREWMAN "rhs_6b48"
-#define HELMET_PILOT "rhs_zsh7a_mike_green_alt"
+#define HELMET "rhsusf_opscore_coy_cover_pelt"
+#define HELMET_CMDR "rhs_8point_marpatwd"
+#define HELMET_RECON "rhs_booniehat2_marpatwd"
+#define HELMET_CREWMAN "rhsusf_cvc_green_ess"
+#define HELMET_PILOT "rhsusf_hgu56p_visor_mask"
 
-#define BACKPACK "rhs_rk_sht_30_emr"
-#define BACKPACK_RADIO "TFAR_mr3000_rhs"
-#define BACKPACK_AR "rhs_tortila_emr"
-#define BACKPACK_AT "rhs_tortila_emr"
-#define BACKPACK_MEDIC "rhs_medic_bag"
+#define BACKPACK "UK3CB_B_Backpack_Pocket"
+#define BACKPACK_RADIO "UK3CB_B_B_Backpack_Radio_Chem"
+#define BACKPACK_AR "B_Carryall_khk"
+#define BACKPACK_AT "B_Carryall_khk"
+#define BACKPACK_MEDIC "UK3CB_B_Backpack_Med"
 #define BACKPACK_CREWMAN BACKPACK_RADIO
 #define BACKPACK_PILOT BACKPACK_RADIO
 
 
-class RHS_UNI_RU_RATNIK_2020 {
+class RHS_UNI_NATO_US_MARINES_RECON_2010 {
   author = "Mazz38";
-  description = "RU: Modern Russian ARMY EMR Camouflage";
+  description = "USA: Modern American MARINE FORCE RECON Camouflage";
 
   class BASE {
     uniform[] = {UNIFORM};
@@ -88,6 +88,7 @@ class RHS_UNI_RU_RATNIK_2020 {
 
   class SNP : BASE {
     headgear[] = {HELMET_RECON};
+    uniform[] = {"UK3CB_TKA_B_U_CombatUniform_Ghillie_DES_MARPAT"};
   };
 
   class CR : BASE {
