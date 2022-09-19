30888daff30219b6e5269f2dd0189b8b735db72d
Martin <gabemartin64@gmail.com>
2022-09-19 14:04:17 -0500

    Update CfgWeapons.hpp

--- addons/fnf_rpk/CfgWeapons.hpp
+++ addons/fnf_rpk/CfgWeapons.hpp
@@ -3,4 +3,7 @@ class CfgWeapons {
 	class rhs_weap_rpk74m: rhs_weap_rpk74n {
 		magazines[] = {"fnf_45rnd_rpk_green"};
 	};
+  class fnf_rpk: rhs_weap_rpk74m {
+    magazines[] = {"fnf_45rnd_rpk_green"};
+  };
 };
