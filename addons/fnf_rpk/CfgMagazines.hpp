dd18b112ecb1d71e6465f2fdca523fc5a10b2412
Martin <gabemartin64@gmail.com>
2022-09-19 13:59:37 -0500

    rpk mod changes
    1. Removed fnf_rpk weapon class
    2. Made fnf rpk magazines only compatible with rhs rpk classes

--- addons/fnf_rpk/CfgMagazines.hpp
+++ addons/fnf_rpk/CfgMagazines.hpp
@@ -1,12 +1,12 @@
 class CfgMagazines {
-    class rhs_45Rnd_545X39_AK_Green;
+  class rhs_45Rnd_545X39_AK_Green;
 	class fnf_45rnd_rpk_green: rhs_45Rnd_545X39_AK_Green {
 		scope = 1;
 		author = "tanaKa";
 		count = 45;
 		descriptionShort = "Caliber: 5.45x39mm<br/>Rounds: 45<br/>Used in: RPK";
-		displayName = "7N22 45 round mag";
+		displayName = "45rnd RPK-74 7T3M (RPK ONLY)";
 		mass = 10;
-		picture = "\rhsafrf\addons\rhs_inventoryicons\data\weapons\rhs_weap_rpk74m_ca.paa";
+		picture = "\rhsafrf\addons\rhs_inventoryicons\data\magazines\rhs_45Rnd_545X39_AK_Green_ca.paa";
 	};
 };
--- addons/fnf_rpk/CfgWeapons.hpp
+++ addons/fnf_rpk/CfgWeapons.hpp
@@ -1,7 +1,6 @@
 class CfgWeapons {
-    class rhs_weap_rpk74m;
-	class fnf_rpk: rhs_weap_rpk74m {
-		author = "tanaKa";
+  class rhs_weap_rpk74n;
+	class rhs_weap_rpk74m: rhs_weap_rpk74n {
 		magazines[] = {"fnf_45rnd_rpk_green"};
 	};
 };
