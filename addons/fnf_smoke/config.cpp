7ad8ff437a78d6ab31191b0e04e048409d9fcacf
Mallen-git <mallen260200@googlemail.com>
2022-12-23 17:43:51 +0000

    Updated Smoke effects to be more effective

--- addons/fnf_smoke/config.cpp
+++ addons/fnf_smoke/config.cpp
@@ -49,7 +49,151 @@ class CfgCloudlets {
 
   class SmokeShellWhite: Default {
 		moveVelocity[]={0.2,0.3,0.1};//{0.2,0.1,0.1}
-		size[]={0.36,3.5,13};//{0.1,2,6}
+		size[]={0.2,3,9};//{0.1,2,6}
 		MoveVelocityVar[]={0.5,0.3,0.5};//{0.25,0.25,0.25}
+  };
+  class SmokeShellRed: SmokeShellWhite {
+    color[] = {{0.6,0.6,0.6,0.5},{0.6,0.6,0.6,0.25},[0.6,0.6,0.6,0]};
+    particleFSFrameCount = 4;
+    particleFSIndex = 12;
+    particleFSLoop = 0;
+    particleFSNtieth = 16;
+    size[] = {0.1,1,5};
+    interval = 0.03;
+  };
+  class SmokeShellGreen: SmokeShellWhite {
+    color[] = {{0.6,0.6,0.6,0.5},{0.6,0.6,0.6,0.25},{0.6,0.6,0.6,0}};
+    particleFSFrameCount = 4;
+    particleFSIndex = 12;
+    particleFSLoop = 0;
+    particleFSNtieth = 16;
+    size[] = {0.1,1,5};
+    interval = 0.03;
+  };
+  class SmokeShellYellow: SmokeShellWhite {
+    color[] = {{0.6,0.6,0.6,0.5},{0.6,0.6,0.6,0.25},{0.6,0.6,0.6,0}};
+    particleFSFrameCount = 4;
+    particleFSIndex = 12;
+    particleFSLoop = 0;
+    particleFSNtieth = 16;
+    size[] = {0.1,1,5};
+    interval = 0.03;
+  };
+  class SmokeShellPurple: SmokeShellWhite {
+    color[] = {{0.6,0.6,0.6,0.5},{0.6,0.6,0.6,0.25},{0.6,0.6,0.6,0}};
+    particleFSFrameCount = 4;
+    particleFSIndex = 12;
+    particleFSLoop = 0;
+    particleFSNtieth = 16;
+    size[] = {0.1,1,5};
+    interval = 0.03;
+  };
+  class SmokeShellBlue: SmokeShellWhite {
+    color[] = {{0.6,0.6,0.6,0.5},{0.6,0.6,0.6,0.25},{0.6,0.6,0.6,0}};
+    particleFSFrameCount = 4;
+    particleFSIndex = 12;
+    particleFSLoop = 0;
+    particleFSNtieth = 16;
+    size[] = {0.1,1,5};
+    interval = 0.03;
+  };
+  class SmokeShellOrange: SmokeShellWhite {
+    color[] = {{0.6,0.6,0.6,0.5},{0.6,0.6,0.6,0.25},{0.6,0.6,0.6,0}};
+    particleFSFrameCount = 4;
+    particleFSIndex = 12;
+    particleFSLoop = 0;
+    particleFSNtieth = 16;
+    size[] = {0.1,1,5};
+    interval = 0.03;
+  };
+};
+
+class SmokeShellRedEffect {
+  class SmokeShell {
+    type = "SmokeShellRed";
+    simulation = "particles";
+    position[] = {0, 0, 0};
+    intensity = 1;
+  };
+  class SmokeShell2 {
+    type = "SmokeShellRed";
+    simulation = "particles";
+    position[] = {0, 0, 0};
+    intensity = 1;
+  };
+};
+
+class SmokeShellGreenEffect {
+  class SmokeShell {
+    type = "SmokeShellGreen";
+    simulation = "particles";
+    position[] = {0, 0, 0};
+    intensity = 1;
+  };
+  class SmokeShell2 {
+    type = "SmokeShellGreen";
+    simulation = "particles";
+    position[] = {0, 0, 0};
+    intensity = 1;
+  };
+};
+
+class SmokeShellYellowEffect {
+  class SmokeShell {
+    type = "SmokeShellYellow";
+    simulation = "particles";
+    position[] = {0, 0, 0};
+    intensity = 1;
+  };
+  class SmokeShell2 {
+    type = "SmokeShellYellow";
+    simulation = "particles";
+    position[] = {0, 0, 0};
+    intensity = 1;
+  };
+};
+
+class SmokeShellPurpleEffect {
+  class SmokeShell {
+    type = "SmokeShellPurple";
+    simulation = "particles";
+    position[] = {0, 0, 0};
+    intensity = 1;
+  };
+  class SmokeShell2 {
+    type = "SmokeShellPurple";
+    simulation = "particles";
+    position[] = {0, 0, 0};
+    intensity = 1;
+  };
+};
+
+class SmokeShellBlueEffect {
+  class SmokeShell {
+    type = "SmokeShellBlue";
+    simulation = "particles";
+    position[] = {0, 0, 0};
+    intensity = 1;
+  };
+  class SmokeShell2 {
+    type = "SmokeShellBlue";
+    simulation = "particles";
+    position[] = {0, 0, 0};
+    intensity = 1;
+  };
+};
+
+class SmokeShellOrangeEffect {
+  class SmokeShell {
+    type = "SmokeShellOrange";
+    simulation = "particles";
+    position[] = {0, 0, 0};
+    intensity = 1;
+  };
+  class SmokeShell2 {
+    type = "SmokeShellOrange";
+    simulation = "particles";
+    position[] = {0, 0, 0};
+    intensity = 1;
   };
 };
