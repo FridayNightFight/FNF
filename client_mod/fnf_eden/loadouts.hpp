c22eb1913320e139fa778e1ef54d4759b638bea7
Mallen-git <mallen260200@googlemail.com>
2023-11-24 01:00:35 +0000

    Added Kits

--- client_mod/fnf_eden/loadouts.hpp
+++ client_mod/fnf_eden/loadouts.hpp
@@ -7,6 +7,15 @@
 			displayName = "US Marines Woodland [2020]";
 			useSideColorOnIcon = 1;
 		};
+    class fnf_USArmy1980
+		{
+			path = "fnf_eden\kits_blufor\USArmy[1980]";
+			side = 8;
+			editorCategory = "fnf_Kits";
+			editorSubcategory = "fnf_Blufor";
+			displayName = "US Army [1980]";
+			useSideColorOnIcon = 1;
+		};
     class fnf_RussianRatnikWoodland2020
 		{
 			path = "fnf_eden\kits_opfor\RussianRatnikWoodland[2020]";
@@ -14,6 +23,15 @@
 			editorCategory = "fnf_Kits";
 			editorSubcategory = "fnf_Opfor";
 			displayName = "Russian Ratnik Woodland [2020]";
+			useSideColorOnIcon = 1;
+		};
+    class fnf_CommunistMilitia1980
+		{
+			path = "fnf_eden\kits_opfor\CommunistMilitia[1980]";
+			side = 8;
+			editorCategory = "fnf_Kits";
+			editorSubcategory = "fnf_Opfor";
+			displayName = "Communist Militia [1980]";
 			useSideColorOnIcon = 1;
 		};
     class fnf_INDFORSyrianArmy2020_indfor
