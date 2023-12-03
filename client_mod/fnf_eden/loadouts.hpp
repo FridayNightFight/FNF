c3000ec89855766ec359d63449adee9c93d4db0b
Mallen-git <mallen260200@googlemail.com>
2023-12-03 00:29:34 +0000

    Re-Organised Kit Structure / Added 12 Kits

--- client_mod/fnf_eden/loadouts.hpp
+++ client_mod/fnf_eden/loadouts.hpp
@@ -1,6 +1,15 @@
+    class fnf_GermanBundeswehr2010
+		{
+			path = "fnf_eden\kits\GermanBundeswehr[2010]";
+			side = 8;
+			editorCategory = "fnf_Kits";
+			editorSubcategory = "fnf_Blufor";
+			displayName = "German Bundeswehr [2010]";
+			useSideColorOnIcon = 1;
+		};
     class fnf_USMarinesWoodland2020
 		{
-			path = "fnf_eden\kits_blufor\USMarinesWoodland[2020]";
+			path = "fnf_eden\kits\USMarinesWoodland[2020]";
 			side = 8;
 			editorCategory = "fnf_Kits";
 			editorSubcategory = "fnf_Blufor";
@@ -9,16 +18,52 @@
 		};
     class fnf_USArmy1980
 		{
-			path = "fnf_eden\kits_blufor\USArmy[1980]";
+			path = "fnf_eden\kits\USArmy[1980]";
 			side = 8;
 			editorCategory = "fnf_Kits";
 			editorSubcategory = "fnf_Blufor";
 			displayName = "US Army [1980]";
 			useSideColorOnIcon = 1;
 		};
+    class fnf_GreekForces2010
+		{
+			path = "fnf_eden\kits\GreekForces[2010]";
+			side = 8;
+			editorCategory = "fnf_Kits";
+			editorSubcategory = "fnf_Blufor";
+			displayName = "Greek Forces [2010]";
+			useSideColorOnIcon = 1;
+		};
+    class fnf_DutchArmy2000
+		{
+			path = "fnf_eden\kits\DutchArmy[2000]";
+			side = 8;
+			editorCategory = "fnf_Kits";
+			editorSubcategory = "fnf_Blufor";
+			displayName = "Dutch Army [2000]";
+			useSideColorOnIcon = 1;
+		};
+    class fnf_USArmy2020
+		{
+			path = "fnf_eden\kits\USArmy[2020]";
+			side = 8;
+			editorCategory = "fnf_Kits";
+			editorSubcategory = "fnf_Blufor";
+			displayName = "US Army [2020]";
+			useSideColorOnIcon = 1;
+		};
+    class fnf_RussianCossaks2010
+		{
+			path = "fnf_eden\kits\RussianCossaks[2010]";
+			side = 8;
+			editorCategory = "fnf_Kits";
+			editorSubcategory = "fnf_Opfor";
+			displayName = "Russian Cossaks [2010]";
+			useSideColorOnIcon = 1;
+		};
     class fnf_RussianRatnikWoodland2020
 		{
-			path = "fnf_eden\kits_opfor\RussianRatnikWoodland[2020]";
+			path = "fnf_eden\kits\RussianRatnikWoodland[2020]";
 			side = 8;
 			editorCategory = "fnf_Kits";
 			editorSubcategory = "fnf_Opfor";
@@ -27,19 +72,73 @@
 		};
     class fnf_CommunistMilitia1980
 		{
-			path = "fnf_eden\kits_opfor\CommunistMilitia[1980]";
+			path = "fnf_eden\kits\CommunistMilitia[1980]";
 			side = 8;
 			editorCategory = "fnf_Kits";
 			editorSubcategory = "fnf_Opfor";
 			displayName = "Communist Militia [1980]";
 			useSideColorOnIcon = 1;
 		};
-    class fnf_INDFORSyrianArmy2020_indfor
+    class fnf_RussianRatnikDesert2020
 		{
-			path = "fnf_eden\kits_indfor\[INDFOR]SyrianArmy(2020)";
+			path = "fnf_eden\kits\RussianRatnikDesert[2020]";
+			side = 8;
+			editorCategory = "fnf_Kits";
+			editorSubcategory = "fnf_Opfor";
+			displayName = "Russian Ratnik Desert [2020]";
+			useSideColorOnIcon = 1;
+		};
+    class fnf_RussianRatnikWinter2020
+		{
+			path = "fnf_eden\kits\RussianRatnikWinter[2020]";
+			side = 8;
+			editorCategory = "fnf_Kits";
+			editorSubcategory = "fnf_Opfor";
+			displayName = "Russian Ratnik Winter [2020]";
+			useSideColorOnIcon = 1;
+		};
+    class fnf_RussianSpetsnaz2010
+		{
+			path = "fnf_eden\kits\RussianSpetsnaz[2010]";
+			side = 8;
+			editorCategory = "fnf_Kits";
+			editorSubcategory = "fnf_Opfor";
+			displayName = "Russian Spetsnaz [2010]";
+			useSideColorOnIcon = 1;
+		};
+    class fnf_Insurgents2000
+		{
+			path = "fnf_eden\kits\Insurgents[2000]";
 			side = 8;
 			editorCategory = "fnf_Kits";
 			editorSubcategory = "fnf_Indfor";
-			displayName = "[INDFOR] Syrian Army (2020)";
+			displayName = "Insurgents [2000]";
+			useSideColorOnIcon = 1;
+		};
+    class fnf_IraqiArmy2000
+		{
+			path = "fnf_eden\kits\IraqiArmy[2000]";
+			side = 8;
+			editorCategory = "fnf_Kits";
+			editorSubcategory = "fnf_Indfor";
+			displayName = "Iraqi Army [2000]";
+			useSideColorOnIcon = 1;
+		};
+    class fnf_MEC2010
+		{
+			path = "fnf_eden\kits\MEC[2010]";
+			side = 8;
+			editorCategory = "fnf_Kits";
+			editorSubcategory = "fnf_Indfor";
+			displayName = "MEC [2010]";
+			useSideColorOnIcon = 1;
+		};
+    class fnf_NAVSOG2010
+		{
+			path = "fnf_eden\kits\NAVSOG[2010]";
+			side = 8;
+			editorCategory = "fnf_Kits";
+			editorSubcategory = "fnf_Indfor";
+			displayName = "NAVSOG [2010]";
 			useSideColorOnIcon = 1;
 		};
