9a1e155060e6545cf3119671dccd664088808c77
Mallen-git <mallen260200@googlemail.com>
2024-05-19 21:15:31 +0100

    Changed: mod switched to specific Wargames mod

---
+++ client_mod/fnf_wargames_eden/config.cpp
@@ -0,0 +1,12 @@
+class CfgPatches
+{
+  class fnf_wargames_eden
+  {
+    units[] = {};
+    weapons[] = {};
+    requiredVersion = 0.1;
+    requiredAddons[] = {"A3_Modules_F", "cba_main", "ace_arsenal", "fnf_eden"}; //This is just to get the FNF section to the bottom of the attributes list
+  };
+};
+
+#include "modules.hpp"
---
+++ client_mod/fnf_wargames_eden/modules.hpp
@@ -0,0 +1,39 @@
+class CfgVehicles
+{
+	class Logic;
+	class Module_F: Logic
+	{
+		class AttributesBase
+		{
+			class Default;
+			class Edit;
+			class Combo;
+			class Checkbox;
+			class CheckboxNumber;
+			class ModuleDescription;
+			class Units;
+		};
+
+		class ModuleDescription
+		{
+			class AnyBrain;
+		};
+	};
+
+  class fnf_module_safeZone: Module_F
+	{
+    class Attributes: AttributesBase
+    {
+      _switchToNonRestrictive = _x getVariable ["fnf_switchToNonRestrictive", false];
+      class SwitchToNonRestrictive: Checkbox
+      {
+        property = "fnf_switchToNonRestrictive";
+        expression = "_this setVariable ['fnf_switchToNonRestrictive',_value, true];";
+        displayName = "Switch to Non-Restrictive";
+        tooltip = "Whether the zone switches to Non-Restrictive mode on deletion, all standard restrictions apply regarding weapon disables, however player can leave the zone";
+        typeName = "BOOL";
+        defaultValue = "false";
+      };
+    };
+	};
+};
