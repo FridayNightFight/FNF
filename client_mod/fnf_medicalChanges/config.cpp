afe5686659fb19306d6c278e4161dd56677cbe3f
Mallen-git <mallen260200@googlemail.com>
2025-09-08 17:08:12 +0100

    Changed: Removed ability for bullets to cause bruises

---
+++ client_mod/fnf_medicalChanges/config.cpp
@@ -0,0 +1,18 @@
+class CfgPatches
+{
+	class fnf_medicalChanges
+	{
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] = {"ace_medical_damage"};
+	};
+};
+
+class ACE_Medical_Injuries {
+	class damageTypes {
+		class bullet {
+			delete Contusion;
+		};
+	};
+};
