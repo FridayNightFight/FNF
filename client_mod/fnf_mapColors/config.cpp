8ebeffc6d1b9f24a61f85be0cdbaf9f39d85d477
Mallen-git <mallen260200@googlemail.com>
2026-04-21 23:16:38 +0100

    Added: Vastly more map colours

---
+++ client_mod/fnf_mapColors/config.cpp
@@ -0,0 +1,133 @@
+class CfgPatches
+{
+	class fnf_mapColors
+	{
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] = {"A3_Functions_F"};
+	};
+};
+
+class CfgMarkerColors {
+	class Default;
+	class ColorBlack;
+	class ColorGrey: Default
+	{
+		name = "Grey";
+		color[] = {0.5,0.5,0.5,1};
+		scope = 2;
+	};
+	class ColorRed;
+	class ColorBrown: Default
+	{
+		name = "Brown";
+		color[] = {0.5,0.25,0,1};
+		scope = 2;
+	};
+	class ColorOrange: Default
+	{
+		name = "Orange";
+		color[] = {0.85,0.4,0,1};
+		scope = 2;
+	};
+	class ColorYellow;
+	class ColorKhaki: Default
+	{
+		name = "Khaki";
+		color[] = {0.5,0.6,0.4,1};
+		scope = 2;
+	};
+	class ColorGreen;
+	class ColorBlue;
+	class ColorPink: Default
+	{
+		name = "Pink";
+		color[] = {1,0.3,0.4,1};
+		scope = 2;
+	};
+	class ColorWhite;
+	class ColorWEST;
+	class ColorEAST;
+	class ColorGUER;
+	class ColorCIV;
+	class ColorUNKNOWN;
+	class ColorNeonCyan: Default
+	{
+		name = "Neon Cyan";
+		color[] = {0.0, 1.0, 1.0, 1.0};
+		scope = 2;
+	};
+	class ColorBurntOrange: Default
+	{
+		name = "Burnt Orange";
+		color[] = {0.8, 0.33, 0.0, 1.0};
+		scope = 2;
+	};
+	class ColorForestGreen: Default
+	{
+		name = "Forest Green";
+		color[] = {0.13, 0.55, 0.13, 1.0};
+		scope = 2;
+	};
+	class ColorDeepViolet: Default
+	{
+		name = "Deep Violet";
+		color[] = {0.5, 0.0, 0.5, 1.0};
+		scope = 2;
+	};
+	class ColorTeal: Default
+	{
+		name = "Teal";
+		color[] = {0.0, 0.5, 0.5, 1.0};
+		scope = 2;
+	};
+	class ColorTanSand: Default
+	{
+		name = "Tan/Sand";
+		color[] = {0.82, 0.71, 0.55, 1.0};
+		scope = 2;
+	};
+	class ColorInfraredRed: Default
+	{
+		name = "Infrared Red";
+		color[] = {0.5, 0.0, 0.0, 0.7};
+		scope = 2;
+	};
+	class ColorGold: Default
+	{
+		name = "Gold";
+		color[] = {1.0, 0.84, 0.0, 1.0};
+		scope = 2;
+	};
+	class ColorCrimson: Default
+	{
+		name = "Crimson";
+		color[] = {0.6, 0.0, 0.0, 1.0};
+		scope = 2;
+	};
+	class ColorTurquoise: Default
+	{
+		name = "Turquoise";
+		color[] = {0.25, 0.88, 0.82, 1.0};
+		scope = 2;
+	};
+	class ColorMagenta: Default
+	{
+		name = "Magenta";
+		color[] = {1.0, 0.0, 1.0, 1.0};
+		scope = 2;
+	};
+	class ColorOliveDrab: Default
+	{
+		name = "Olive Drab";
+		color[] = {0.42, 0.56, 0.14, 1.0};
+		scope = 2;
+	};
+	class ColorLavender: Default
+	{
+		name = "Lavender";
+		color[] = {0.9, 0.75, 1.0, 1.0};
+		scope = 2;
+	};
+};
